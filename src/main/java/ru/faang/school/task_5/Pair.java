70d0530ba32a9c59315074904fab017891cc9793
OneWayMy <zhenya.ivashchenko.9494@gmail.com>
2023-06-08 15:42:38 -0700

    Revert "Task 5 to Ru names"
    This reverts commit 8c6ec787728676c2dc3a4d30b4fcb315a320ea75.

--- src/main/java/ru/faang/school/task_5/Pair.java
+++ src/main/java/ru/faang/school/task_5/Pair.java
@@ -40,6 +40,6 @@ public class Pair {
 
     @Override
     public String toString() {
-        return "Группа: " + faculty + ",Курс: " + year;
+        return "Группа: " + faculty + ",курc " + year;
     }
 }
