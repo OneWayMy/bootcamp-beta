af54bc7e654bf5ed610716a85b08275ba3b785ae
Pavel <pavlik1123.pg@gmail.com>
2023-06-10 13:44:43 +0700

    add hashmap_task3

---
+++ src/main/java/ru/faang/school/hashmap/task_3/student/GrupStudent.java
@@ -0,0 +1,31 @@
+package ru.faang.school.hashmap.task_3.student;
+
+import java.util.Objects;
+
+public class GrupStudent {
+    private final String faculty;
+    private final int year;
+
+    public GrupStudent(String faculty, int year) {
+        this.faculty = faculty;
+        this.year = year;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GrupStudent that = (GrupStudent) o;
+        return year == that.year && Objects.equals(faculty, that.faculty);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(faculty, year);
+    }
+
+    @Override
+    public String toString() {
+        return "Фаакультет: " + faculty + " курс: " + year;
+    }
+}
